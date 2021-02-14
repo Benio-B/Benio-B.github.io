8e0a9e62493495ab51afd602eb93665696fbd350
BenB-Enalean <benjamin.bouillot@enalean.com>
2021-02-14 10:38:59 +0100

    Init benio project.
    Use Vue, Typescript, Babel, Sass and Eslint

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="main-page">
+        <font-awesome-icon icon="bars" />
+        Good morning here!
+    </div>
+</template>
+
+<script lang="ts">
+    import { Vue } from 'vue-class-component';
+
+    export default class App extends Vue {}
+</script>
+
+<style lang="scss">
+    @import '@/styles/_main.scss';
+</style>
